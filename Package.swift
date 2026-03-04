6621d323ae6e466ad834f3d3a0c06f54b36c0c9c
Justin Williams <justinw@me.com>
2026-03-04 09:55:59 -0700

    Update Package.swift to use SwiftLint plugin

--- Package.swift
+++ Package.swift
@@ -1,20 +1,35 @@
-// swift-tools-version:5.4
+// swift-tools-version:6.0
 
 import PackageDescription
 
 let package = Package(
     name: "Package",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
-        .tvOS(.v14)
+        .iOS(.v18),
+        .macOS(.v15)
     ],
     products: [
         .library(name: "Package", targets: ["Package"]),
     ],
-    dependencies: [],
+    dependencies: [
+        // JWW Standard Issue Package set.
+        .package(url: "https://github.com/justin/jww-standard-lib.git", from: "1.0.0"),
+        
+        // SwiftLint build tool plugin for code quality
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.61.0")
+    ],
     targets: [
-        .target( name: "Package"),
-        .testTarget(name: "PackageTests", dependencies: ["Package"])
+        .target(name: "Package",
+                dependencies: [
+                    .product(name: "JWWCore", package: "jww-standard-lib")
+                ],
+                plugins: [
+                    .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
+                ]),
+        .testTarget(name: "PackageTests",
+                    dependencies: [
+                        "Package",
+                        .product(name: "JWWCore", package: "jww-standard-lib"),
+                    ])
     ]
 )
